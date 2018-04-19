0129f93547fff094075b8ca71b4ee2d32bb5abb2
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-19 21:20:01 +0700

    xong

--- c.js
+++ c.js
@@ -11,18 +11,26 @@ console.log(getRandom(3));
 
 /*
 
-*****
- ****
-  ***
-   **
-    *
-
+     *
+    ***
+   *****
+  *******
+ *********
+***********
+ *********
+  *******
+   *****
+    ***
+     *
 */
-for(let i = 1; i <= 5; i++) {
+for(let i = 1; i <= 11; i++) {
     let str = '';
-    for(let j = 1; j <= 5; j++) {
-        const dk = i <= j;
+    for(let j = 1; j <= 11; j++) {
+        const soBuocNgang = Math.abs(j - 6);
+        const soBuocDoc = Math.abs(i - 6);
+        const dk = soBuocDoc + soBuocNgang <= 5;
         str += (dk ? '*' : ' ');
     }
     console.log(str);
 }
+//https://github.com/vanpho93/docs/blob/master/BaiTap_JavaScript_NodeJS.pdf
